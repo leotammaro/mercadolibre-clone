15fa288437ff0d396fd944361fa53e3d3d6b02fe
Leonel Tammaro <tammaroleonel@gmail.com>
2021-06-13 14:08:37 -0300

    navbar responsive

--- src/components/navbar/NavbarFullScreen.js
+++ src/components/navbar/NavbarFullScreen.js
@@ -13,45 +13,45 @@ import "./styleCssFullScreen.css"
 
 function NavbarFullScreen() {
     return (
-        <Stack direction="row" justifyContent="center" display={{base:"none",xl:"flex"}}>
-            <Stack>
+        <Stack direction="row" justifyContent="center" display={{base:"none",xl:"flex"}} h="100px">
+            <Stack w="180px" alignItems="center">
                 <Image h="40px" w="130px" src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.14.5/mercadolibre/logo__large_plus@2x.png" />
                 <Stack  direction="row" alignItems="center" h="40px">
                     <Image src={Map} h="30px" opacity="0.7"/>
-                    <Stack direction="column" spacing={0} fontSize="13px">
+                    <Stack direction="column" spacing={0} fontSize="12px">
                         <Text className="text">Enviar a LEONEL</Text>
                         <Text>ISLA TRINIDAD 390</Text>
                     </Stack>
                 </Stack>
             </Stack>  
             <Stack padding="0px 40px" direction="column">
-                <Stack position="relative" bg="white" direction="row" alignItems="center" >
-                    <Input w="100%" border="none"  placeholder="Buscar productos, marcas y mas..." boxShadow="0 1px 2px 0 rgb(0,0,0,0.2)" borderRadius="0" ></Input>
+                <Stack position="relative" bg="white" direction="row" alignItems="center" w="100%" >
+                    <Input w="100%" border="none"  placeholder="Buscar productos, marcas y más..." boxShadow="0 1px 2px 0 rgb(0,0,0,0.2)" borderRadius="0" ></Input>
                     <Image w="20px" src={Search} position="absolute" right="1"></Image>
                 </Stack>
-                <Stack direction="row" w="80%" justifyContent="flex-start">
-                    <Stack direction="row"  alignItems="center" position="relative" justifyContent="flex-start">
-                        <Button className="buttonNavbar" fontSize="13px" bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}}>Categorias</Button>
-                        <Image src={DownSearch} h="20px" position="absolute" right={-1} top={3}/>
+                <Stack direction="row" w="500px" spacing={3} justifyContent="flex-start">
+                    <Stack direction="row"  alignItems="center" position="relative" justifyContent="flex-start" >
+                        <Button  fontWeight="200" fontSize="13px" bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}} paddingLeft={0}>Categorias</Button>
+                        <Image src={DownSearch} opacity="0.6" h="20px" position="absolute" right={-1} top={3}/>
                     </Stack>
-                    <Button fontSize="13px"  bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}}>Ofertas</Button>
-                    <Button fontSize="13px"  bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}}>Historial</Button>
-                    <Button  fontSize="13px" bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}}>Supermercado</Button>
-                    <Button fontSize="13px"  bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}}>Historial</Button>
-                    <Button fontSize="13px" bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}}>Moda</Button>
-                    <Button fontSize="13px" bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}}>Vender</Button>
+                    <Button  fontSize="13px" fontWeight="200"  bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}} padding={0}>Ofertas</Button>
+                    <Button fontSize="13px" fontWeight="200"  bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}} padding={0}>Historial</Button>
+                    <Button  fontSize="13px" fontWeight="200" bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}} padding={0}>Supermercado</Button>
+                    <Button fontSize="13px" fontWeight="200"  bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}} padding={0}>Historial</Button>
+                    <Button fontSize="13px" fontWeight="200" bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}} padding={0}>Moda</Button>
+                    <Button fontSize="13px" fontWeight="200" bg="none" _hover={{bg:"none"}} _active={{bg:"none"}} _focus={{borderColor:"none"}} padding={0}>Vender</Button>
                 </Stack>
             </Stack>
-            <Stack direction="column" justifyContent="space-around">
-                <Text>Hasta 4 meses de regalo en Disney+</Text>
-                <Stack direction="row">
-                    <Stack direction="row">
+            <Stack direction="column" justifyContent="space-around" alignItems="flex-start" w="350px">
+                <Text fontWeight="600">Hasta 4 meses de regalo en Disney+</Text>
+                <Stack direction="row" fontSize="13px" spacing={3}>
+                    <Stack direction="row" spacing={1}>
                         <Image src={User} h="20px"/>
-                        <Text>Leonel</Text>
+                        <Text >Leonel</Text>
                         <Image src={DownSearch} h="20px"/>
                     </Stack>
                     <Text>Mis compras</Text>
-                    <Stack direction="row">
+                    <Stack direction="row" spacing={1}>
                         <Text>Favoritos</Text>
                         <Image src={DownSearch} h="20px"/>
                     </Stack>
